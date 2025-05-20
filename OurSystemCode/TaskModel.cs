1731cd7d7b23a9ec036ca79b6b6d6552fbf75b0f
unknown <raghadf2>
2025-05-21 00:47:57 +0300

    add my project

---
+++ OurSystemCode/TaskModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OurSystemCode
+{
+    public class TaskModel
+    {
+        public string TaskName { get; set; }
+        public string AssignedTo { get; set; }
+        public string Status { get; set; }
+        public DateTime Deadline { get; set; }
+    }
+}
